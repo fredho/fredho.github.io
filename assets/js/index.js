b9cdb1ac868ed15d579fa373d1a2033c374a75f5
fred.ho <frederick.ho.1994@gmail.com>
2019-07-14 21:54:45 -0700

    stuff

---
+++ assets/js/index.js
@@ -0,0 +1,79 @@
+$(document)
+  .ready(function() {
+
+    var
+      changeSides = function() {
+        $('.ui.shape')
+          .eq(0)
+            .shape('flip over')
+            .end()
+          .eq(1)
+            .shape('flip over')
+            .end()
+          .eq(2)
+            .shape('flip back')
+            .end()
+          .eq(3)
+            .shape('flip back')
+            .end()
+        ;
+      },
+      validationRules = {
+        firstName: {
+          identifier  : 'email',
+          rules: [
+            {
+              type   : 'empty',
+              prompt : 'Please enter an e-mail'
+            },
+            {
+              type   : 'email',
+              prompt : 'Please enter a valid e-mail'
+            }
+          ]
+        }
+      }
+    ;
+
+    $('.ui.dropdown')
+      .dropdown({
+        on: 'hover'
+      })
+    ;
+
+    $('.ui.form')
+      .form(validationRules, {
+        on: 'blur'
+      })
+    ;
+
+    $('.masthead .information')
+      .transition('scale in', 1000)
+    ;
+
+    $('.launch.icon').click(function() {
+      $('.ui.sidebar')
+        .sidebar('toggle')
+      ;      
+    });
+
+    setInterval(changeSides, 3000);
+
+   $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top - 100
+            }, 1200);
+            $("body").trigger("click");
+            return false;
+        }
+    }
+});
+
+  })
+;
